1e531803d400e149f69e0d6e2d012e36f4af1bbc
DESKTOP-TGB9Q1C\Revo <radoslawrak@outlook.com>
2021-11-30 23:41:32 +0100

    initial commit

---
+++ Assets/InteractionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionController : MonoBehaviour
+{
+    public GameObject kwadrat;
+    public GameObject kulka;
+
+    public string TextToDisplay = "Click 'E' to interact";
+    public Text textField;
+
+    public myEnum InteractionType = new myEnum();
+    public enum myEnum
+    {
+        Animation,
+        ObjectStateChange,
+        Item3
+    };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+           textField.text = TextToDisplay;
+           kwadrat.SetActive(false);
+           kulka.SetActive(true);
+        }
+            
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textField.text = "";
+            kwadrat.SetActive(true);
+            kulka.SetActive(false);
+        }
+    }
+}
---
+++ Assets/TimeSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class TimeSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private string DaySceneName;
+    [SerializeField]
+    private string NightScenename;
+
+    public string TextToDisplay = "Click 'E' to interact";
+    public Text textField;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textField.text = TextToDisplay;
+
+            if (Input.GetKeyDown("e"))
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                if (scene.name == DaySceneName)
+                {
+                    SceneManager.LoadScene(NightScenename);
+                }
+                else
+                {
+                    SceneManager.LoadScene(DaySceneName);
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        textField.text = "";
+    }
+}
